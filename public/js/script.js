82e515251bce118b0eac09208332af1df0712a7a
vyxiel <vyxiel.vous@gmail.com>
2020-04-23 02:36:09 +0700

    mousemove v.1

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,14 @@
-let camera, scene, renderer;
-let cube;
+let camera, 
+	scene, 
+	renderer,
+	cube;
+
+let mouseDown = false,
+    mouseX = 0,
+    mouseY = 0,
+    deltaX = 0,
+    deltaY = 0;
+
 
 function init(){
 
@@ -17,16 +26,16 @@ function init(){
 	const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
 	const material = new THREE.MeshNormalMaterial( { color: 0x333333 } );
 
-	// for ( let i = 0; i <= 10; i ++ ) {
-    
-    	cube = new THREE.Mesh( geometry, material );
-      	cube.position.y = 5;
-    	scene.add( cube );
-    // }
+	cube = new THREE.Mesh( geometry, material );
+  	cube.position.y = 5;
+	scene.add( cube );
 
+    window.addEventListener( 'mousemove', onMouseMove, false );
+    window.addEventListener( 'mousedown', onMouseDown, false);
+    window.addEventListener( 'mouseup', onMouseUp, false);
    	window.addEventListener( 'wheel', onMouseWheel, false );
    	window.addEventListener( 'resize', onWindowResize, false );
-   	
+
 }
 
 function animate() {
@@ -34,6 +43,42 @@ function animate() {
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
 
+}
+
+function onMouseMove ( evt ){
+    if (!mouseDown) {
+        return;
+    }
+
+    evt.preventDefault();
+
+    deltaX = evt.clientX - mouseX;
+    deltaY = evt.clientY - mouseY;
+
+    mouseX = evt.clientX;
+    mouseY = evt.clientY;
+
+    // console.log("deltaX : "+deltaX);
+    // console.log("deltaY : "+deltaY);
+    // console.log("mouseX : "+mouseX);
+    // console.log("mouseY : "+mouseY);
+
+    rotateScene(deltaX, deltaY);
+
+}
+
+function onMouseDown(evt) {
+    evt.preventDefault();
+
+    mouseDown = true;
+    mouseX = evt.clientX;
+    mouseY = evt.clientY;
+}
+
+function onMouseUp(evt) {
+    evt.preventDefault();
+
+    mouseDown = false;
 }
 
 function onMouseWheel( event ) {
@@ -58,6 +103,11 @@ function onWindowResize() {
 	camera.updateProjectionMatrix();
 	renderer.setSize( window.innerWidth, window.innerHeight );
 
+}
+
+function rotateScene(deltaX, deltaY) {
+    cube.rotation.y += deltaX / 100;
+    cube.rotation.x += deltaY / 100;
 }
 
 init();
