ac5489b89b5159afeaf46b4bfe37b201fd2ee275
vyxiel <vyxiel.vous@gmail.com>
2020-04-25 01:42:55 +0700

    Light Testing

--- public/js/script.js
+++ public/js/script.js
@@ -1,39 +1,87 @@
 let camera, 
-	scene, 
+	scene,
+	controls,
 	renderer,
 	cube;
 
 let mouseDown = false,
-    mouseX = 0,
-    mouseY = 0,
+    mouseX = window.innerWidth/2,
+    mouseY = window.innerHeight/2,
     deltaX = 0,
     deltaY = 0;
 
+init();
+animate();
 
 function init(){
 
 	scene = new THREE.Scene();
-	scene.background = new THREE.Color(0xffffff)
+	scene.background = new THREE.Color(0xffffff);
+	scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
 
-	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-    camera.position.set( 0, 5, 2 );
+	// camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+	// camera.position.set( 0, 5, 2 );
 
 	renderer = new THREE.WebGLRenderer( { antialias: true } );
 	renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild( renderer.domElement );
 
-	const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-	const material = new THREE.MeshNormalMaterial( { color: 0x333333 } );
+	camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
+	camera.position.set( 400, 100, 0 );
 
-	cube = new THREE.Mesh( geometry, material );
-  	cube.position.y = 5;
-	scene.add( cube );
+	/*controls = new MapControls( camera, renderer.domElement );
 
-    window.addEventListener( 'mousemove', onMouseMove, false );
-    window.addEventListener( 'mousedown', onMouseDown, false);
-    window.addEventListener( 'mouseup', onMouseUp, false);
-   	window.addEventListener( 'wheel', onMouseWheel, false );
+	//controls.addEventListener( 'change', render ); // call this only in static scenes (i.e., if there is no animation loop)
+
+	controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+	controls.dampingFactor = 0.05;
+
+	controls.screenSpacePanning = false;
+
+	controls.minDistance = 100;
+	controls.maxDistance = 500;
+
+	controls.maxPolarAngle = Math.PI / 2;*/
+
+
+	const geometry = new THREE.BoxGeometry(1, 1, 1);
+	geometry.translate( 0, 0.5, 0 );
+	const material = new THREE.MeshPhongMaterial( { color: 0xffffff, flatShading: true } );
+
+	for ( var i = 0; i < 500; i ++ ) {
+
+		var mesh = new THREE.Mesh( geometry, material );
+		mesh.position.x = Math.random() * 1600 - 800;
+		mesh.position.y = 0;
+		mesh.position.z = Math.random() * 1600 - 800;
+		mesh.scale.x = 20;
+		mesh.scale.y = Math.random() * 80 + 10;
+		mesh.scale.z = 20;
+		mesh.updateMatrix();
+		mesh.matrixAutoUpdate = false;
+		scene.add( mesh );
+	}
+
+	// cube = new THREE.Mesh( geometry, material );
+ 	// cube.position.y = 5;
+	// scene.add( cube );
+
+	var light = new THREE.DirectionalLight( 0xffffff );
+	light.position.set( 1, 1, 1 );
+	scene.add( light );
+
+	var light = new THREE.DirectionalLight( 0x002288 );
+	light.position.set( - 1, - 1, - 1 );
+	scene.add( light );
+
+	var light = new THREE.AmbientLight( 0x222222 );
+	scene.add( light );
+
+    // window.addEventListener( 'mousemove', onMouseMove, false );
+    // window.addEventListener( 'mousedown', onMouseDown, false);
+    // window.addEventListener( 'mouseup', onMouseUp, false);
+   	// window.addEventListener( 'wheel', onMouseWheel, false );
    	window.addEventListener( 'resize', onWindowResize, false );
 
 }
@@ -41,14 +89,15 @@ function init(){
 function animate() {
 
 	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
+	render();
 
 }
 
 function onMouseMove ( evt ){
-    if (!mouseDown) {
-        return;
-    }
+	/* Hold Mouse 1 */
+    // if (!mouseDown) {
+    //     return;
+    // }
 
     evt.preventDefault();
 
@@ -57,11 +106,6 @@ function onMouseMove ( evt ){
 
     mouseX = evt.clientX;
     mouseY = evt.clientY;
-
-    // console.log("deltaX : "+deltaX);
-    // console.log("deltaY : "+deltaY);
-    // console.log("mouseX : "+mouseX);
-    // console.log("mouseY : "+mouseY);
 
     rotateScene(deltaX, deltaY);
 
@@ -106,9 +150,12 @@ function onWindowResize() {
 }
 
 function rotateScene(deltaX, deltaY) {
-    cube.rotation.y += deltaX / 100;
-    cube.rotation.x += deltaY / 100;
+    cube.rotation.y += deltaX / 1000;
+    cube.rotation.x += deltaY / 1000;
 }
 
-init();
-animate();
+function render() {
+
+	renderer.render( scene, camera );
+
+}	
