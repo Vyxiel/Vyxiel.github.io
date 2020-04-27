3ba49b45795c0dbaedb7338ba3aaf402d9de6cfa
vyxiel <vyxiel.vous@gmail.com>
2020-04-27 15:48:12 +0700

    Floor Added

--- public/js/script.js
+++ public/js/script.js
@@ -3,7 +3,7 @@ let camera,
 	controls,
 	renderer,
 	raycaster,
-	cube;
+	meshes;
 
 let mouseDown = false,
 	mouse,
@@ -19,8 +19,8 @@ animate();
 function init(){
 
 	scene = new THREE.Scene();
-	scene.background = new THREE.Color(0xffffff);
-	scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
+	scene.background = new THREE.Color(0x333333);
+	scene.fog = new THREE.FogExp2( 0x333333, 0.002 );
 
 	// camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 	// camera.position.set( 0, 5, 2 );
@@ -28,15 +28,33 @@ function init(){
 	renderer = new THREE.WebGLRenderer( { antialias: true } );
 	renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.setSize( window.innerWidth, window.innerHeight );
+	// renderer.shadowMap.enabled = true;
+	// renderer.shadowMap.type = THREE.BasicShadowMap;
 	document.body.appendChild( renderer.domElement );
 
-	camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
-	camera.position.set( 400, 300, 0 );
-	camera.rotation.x = Math.PI * Math.sin( 60 );
+
+	camera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 1, 1000 );
+	camera.position.set( 200, 300, 200 );
+	// camera.rotation.order = 'YXZ';
+	camera.rotation.set( Math.PI/180 *  300 , Math.PI/180 *  30 , Math.PI/180 *  30);
 
 	raycaster = new THREE.Raycaster();
 	mouse = new THREE.Vector2();
 
+	/*lighting*/
+	var light = new THREE.DirectionalLight( 0xffffff );
+	light.position.set( - 1, 1, 1 );
+	scene.add( light );
+
+	var light = new THREE.DirectionalLight( 0x002288 );
+	light.position.set( - 1, 1, 1 );
+	scene.add( light );
+
+	// var light = new THREE.AmbientLight( 0x222222 );
+	// light.castShadow = true;
+	// scene.add( light );
+
+	meshes = new THREE.Object3D();
 	const geometry = new THREE.BoxGeometry(1, 1, 1);
 	geometry.translate( 0, 0.5, 0 );
 
@@ -51,24 +69,21 @@ function init(){
 		mesh.scale.z = 20;
 		mesh.updateMatrix();
 		mesh.matrixAutoUpdate = false;
-		scene.add( mesh );
-	
+		// mesh.receiveShadow = true;
+		// mesh.castShadow = true;
+		meshes.add( mesh );
 	}
 
-	// cube = new THREE.Mesh( geometry, material );
- 	// cube.position.y = 5;
-	// scene.add( cube );
+	meshFloor = new THREE.Mesh(
+		new THREE.PlaneGeometry( 1600, 1600, 100, 100 ),
+		new THREE.MeshPhongMaterial( { color:0x333333, flatShading: true } )
+	);
+	// meshFloor.receiveShadow = true;
+	meshFloor.position.y = 0;
+	meshFloor.rotation.x -= Math.PI / 2;
 
-	var light = new THREE.DirectionalLight( 0xffffff );
-	light.position.set( 1, 1, 1 );
-	scene.add( light );
-
-	var light = new THREE.DirectionalLight( 0x002288 );
-	light.position.set( - 1, - 1, - 1 );
-	scene.add( light );
-
-	var light = new THREE.AmbientLight( 0x222222 );
-	scene.add( light );
+	scene.add( meshes );
+	scene.add( meshFloor );
 
     window.addEventListener( 'mousemove', onMouseMove, false );
     window.addEventListener( 'mouseout', onMouseOut, false);
@@ -141,11 +156,11 @@ function onMouseWheel( event ) {
 
 	// camera.position.y -= event.deltaY * 0.005;
 
-	cube.rotation.x += 0.05;
-	cube.rotation.y += 0.05;
-	cube.rotation.z -= 0.05;
+	// cube.rotation.x += 0.05;
+	// cube.rotation.y += 0.05;
+	// cube.rotation.z -= 0.05;
   
-  // prevent scrolling beyond a min/max value
+  	// prevent scrolling beyond a min/max value
   
   	camera.position.clampScalar( 0, 10 );
 
@@ -162,8 +177,8 @@ function onWindowResize() {
 function translateCamera(deltaX, deltaY) {
     // scene.rotation.y += deltaX / 1000;
     // scene.rotation.x += deltaY / 1000;
-    camera.position.x -= deltaX / 2;
-    camera.position.z -= deltaY / 2;
+    camera.position.x -= deltaX / 2 + deltaY / 4;
+    camera.position.z -= deltaY / 2 - deltaX / 4;
 }
 
 function render() {
@@ -171,7 +186,7 @@ function render() {
 	raycaster.setFromCamera( mouse, camera );
 
 	// calculate objects intersecting the picking ray
-	var intersects = raycaster.intersectObjects( scene.children );
+	var intersects = raycaster.intersectObjects( meshes.children );
 
     if ( intersects.length > 0 ) {
   		if ( INTERSECTED != intersects[ 0 ].object ) {
